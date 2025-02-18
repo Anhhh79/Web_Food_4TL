07fed3849342501d4a935d8a945464adadf7005b
Văn Hoài <vanhoai1912@gmail.com>
2025-02-18 19:13:15 +0700

    h

---
+++ Web_Food_4TL/Areas/Customer/Controllers/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Food_4TL.Areas.Customer.Controllers
+{
+    public class ShoppingCart : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
