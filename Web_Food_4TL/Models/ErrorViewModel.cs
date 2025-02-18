b34bd8d9643d490781b4856837494f637e3045a5
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-02-18 15:48:54 +0700

    Add project files.

---
+++ Web_Food_4TL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Food_4TL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
