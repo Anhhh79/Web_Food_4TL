dd3c1da22eaf83442312754e1d1966598686b8f6
QuocBao <quocbao272004@gmail.com>
2025-03-07 16:03:56 +0700

    add giao diện ad

--- Web_Food_4TL/Program.cs
+++ Web_Food_4TL/Program.cs
@@ -22,6 +22,5 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-
+    pattern: "{area=Admin}/{controller=Home}/{action=Index}/{id?}");
 app.Run();
